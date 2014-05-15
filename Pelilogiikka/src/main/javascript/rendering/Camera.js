1eca19b4ff7b85e1ab5c8a78e432ba4521183d3c
ttuomala <ttuomala@da1-cs-b221-21.helsinki.fi>
2014-05-15 15:03:51 +0300

    Jag puttar mycket dataprogram upp!

--- Pelilogiikka/src/main/javascript/rendering/Camera.js
+++ Pelilogiikka/src/main/javascript/rendering/Camera.js
@@ -1,9 +1,18 @@
 
 
+    
+    function Camera( position )     
+    {
+      this.position     = position;
+      this.orientiation = mat3.create();
+                          mat3.identity( this.orientation );                  
+    }
 
-function Camera( position ) 
-{
-    this.position     = position;
-    this.orientiation = mat4.create();
-                        mat4.identity( this.orientation );                  
-}
+
+
+    Camera.prototype.get_ViewMatrix = function()
+    {    
+       var inverse_Origin = mat3.transpose();
+            
+       return inverse_Origin;
+    }
