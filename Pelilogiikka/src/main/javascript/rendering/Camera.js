adc43b7067c9d34bd8bd39829c9c922047ac39b2
ttuomala <ttuomala@da1-cs-b221-21.helsinki.fi>
2014-05-15 15:50:07 +0300

    Adding stuff

--- Pelilogiikka/src/main/javascript/rendering/Camera.js
+++ Pelilogiikka/src/main/javascript/rendering/Camera.js
@@ -1,18 +1,45 @@
 
 
     
-    function Camera( position )     
+    function Camera()     
     {
-      this.position     = position;
-      this.orientiation = mat3.create();
-                          mat3.identity( this.orientation );                  
+      this.position     = [0,0,0];
+      this.orientation = mat4.create();
+                         mat4.identity( this.orientation );                  
     }
-
 
 
     Camera.prototype.get_ViewMatrix = function()
     {    
-       var inverse_Origin = mat3.transpose();
-            
-       return inverse_Origin;
+          var inv;
+                  
+                    mat4.transpose( this.orientation, inv );       
+                    mat4[12] = this.position[0];
+                    mat4[13] = this.position[1];
+                    mat4[14] = this.position[2];
+                    
+       return this.orientation; inv;
     }
+   
+  
+    Camera.prototype.pitch = function( radians ) 
+    {
+        mat4.rotate( this.orientation, radians, [1,0,0], 0 );
+    }	
+  
+
+    Camera.prototype.yaw = function( radians )
+    {
+        mat4.rotate( this.orientation, radians, [0,1,0], 0 );
+    }
+
+    Camera.prototype.roll = function( radians ) 
+    {
+        mat4.rotate( this.orientation, radians, [0,0,1], 0 );
+    }
+	
+    Camera.prototype.move = function( displacement )
+    {
+        vec3.add( this.position, displacement, this.position );
+    }
+    
