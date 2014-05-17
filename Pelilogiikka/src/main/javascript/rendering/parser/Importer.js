8ce083f8ddbf425bf8e89be572c452b875590ada
Ubertom <Ubertom@192.168.1.33>
2014-05-17 21:04:48 +0300

    lulz

--- Pelilogiikka/src/main/javascript/rendering/parser/Importer.js
+++ Pelilogiikka/src/main/javascript/rendering/parser/Importer.js
@@ -3,7 +3,7 @@
 
     // <node type="mesh">
     
-    function next_MeshString( rawData, cursor )
+    function next_NodeString( rawData, cursor )
     {
        var begin_Node = rawData.indexOf( '<node type="mesh">' , cursor );
        
@@ -21,44 +21,120 @@
     
     
     
-    function parse_Mesh( descriptor )
+    function parse_Node( descriptor )
     {
          //console.debug( string );
         var lines = descriptor.toString().split("\n");
+       
+        var meshName;
+        var position   = [];
+        var rotation   = [];
         
-        lines.forEach( function( token){
+        
+        var statusFlag = "UNKNOWN"
+        
+        
+        lines.forEach( function( line )
+        {
+            if( line.indexOf("<attributes>") > -1 )
+            {
+                statusFlag = "MESH";
+                return;
+            }
            
-            alert( token );
+            if( line.indexOf("<materials>") > -1 )
+            {
+                statusFlag = "MATERIALS";
+                return;
+            }
+            
+            // This line contains orientational or positional data
+            if( line.indexOf( "<vector3d" ) > -1 )  
+            {
+                var index = line.indexOf('name="Position" value="'); // +22 
+                
+                if( index > -1 ) 
+                {
+                   index += 23;
+                   var data = line.substring( index, line.length-5 );   // Ignore the />
+                   var tokens = data.split( ",");
+                   position =  [ parseFloat( tokens[0] ) ,  parseFloat( tokens[1] ) , parseFloat( tokens[2]) ];
+                }
+                
+                index = line.indexOf('name="Rotation" value=');
+                    
+                if( index > -1 )
+                {
+                   index += 23;
+                   var data = line.substring( index, line.length-5 );   // Ignore the />
+                   var tokens = data.split( ",");
+                   rotation =  [ parseFloat( tokens[0] ) ,  parseFloat( tokens[1] ) , parseFloat( tokens[2]) ];
+                }
+            }
+            else
+                if( line.indexOf( "<string" ) > -1 )
+                {
+                    var index = line.indexOf('name="Mesh" value="'); // +19 
+                       
+                    if( index > -1 )     // Contains the mesh reference
+                    {
+                        index   += 19;
+                        meshName = line.substring( index, line.length-5 );   // Ignore the />
+                    }
+                    
+                    index = line.indexOf('name="Name" value="'); // +19 
+                      
+                    if( index > -1 )     // Contains the name of the node
+                    {
+                        ("LOL");
+                    }
+                }   
         });
+     
         
+    return [ meshName, position, rotation ];
     }
     
     
-    function next_Mesh( rawData, cursor )
+    
+    
+    
+    
+    function next_Node( rawData, cursor )
     {
-        var descriptor = next_MeshString( rawData, cursor );
-        
-        parse_Mesh( descriptor );
+        var descriptor = next_NodeString( rawData, cursor );
+        var node       =  parse_Node( descriptor );
+    
+    return [ node, descriptor[1] ];
     }
     
     
     
+    
+    
+    function build_Node( node_Descriptor )
+    {
+        
+       // data[0];
+       // cursor = data[1];
+        
+    }
     
     
     function import_Scene( gl, fileName )
     {
         var rawData = FileHelper.readStringFromFileAtPath ( fileName );
         
-        var position;
-        var orientation;
+        var cursor = 0;
         
-        rawData.forEach( function( line )
-        {
-            
-        });
+        var descriptor = next_Node( rawData, cursor );
+        var       node = build_Node( descriptor );
         
         
+         
+        alert("Manifest position " + node[1][0] + " : " + node[1][1] + " : " + node[1][2] );
+        alert("Manifest rotation " + node[2][0] + " : " + node[2][1] + " : " + node[2][2] );
+        alert("Manifest mesh     " + node[0]);    
         
-        next_Mesh( rawData, 0 );
         
     }
