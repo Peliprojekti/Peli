221e81282a4be3bb00549be22f05251367ee1088
Ubertom <Ubertom@192.168.1.33>
2014-05-17 04:00:25 +0300

    lulz

---
+++ Pelilogiikka/src/main/javascript/rendering/Importer.js
@@ -0,0 +1,41 @@
+
+
+// Funktio pöllitty täältä: http://stackoverflow.com/questions/13709482/how-to-read-text-file-in-javascript
+// Jos joku tietää paremman keinon, kertokaa toki!
+// Tärkeintä nyt kuitenkin saada Editori -> Export -> Import -> Engine ketju jotenkin kasaan!
+function FileHelper( pathOfFileToReadFrom )
+{}
+{
+    FileHelper.readStringFromFileAtPath = function(pathOfFileToReadFrom)
+    {
+        var request = new XMLHttpRequest();
+        request.open("GET", pathOfFileToReadFrom, false);
+        request.send(null);
+        var returnValue = request.responseText;
+
+        return returnValue;
+    }
+}
+
+
+
+
+
+function import_Mesh( fileName )
+{
+    var text = FileHelper.readStringFromFileAtPath ( fileName );
+    
+    
+    var tokens = text.split(" ");
+    
+    
+    tokens.forEach(function( entry )
+    {
+         console.log( entry );  
+    });
+    
+ 
+    
+    
+}
+
