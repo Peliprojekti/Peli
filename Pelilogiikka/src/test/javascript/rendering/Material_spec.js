d11a2de4aeca5523a0d93e0fefa09773aa3ba6b1
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-05-20 15:51:52 +0300

    Draft versions of unit tests.

---
+++ Pelilogiikka/src/test/javascript/rendering/Material_spec.js
@@ -0,0 +1,16 @@
+describe('the material object', function() {
+
+    //Create an easily-removed container for our tests to play in
+    beforeEach(function() {
+    });
+
+    //Clean it up after each spec
+    afterEach(function() {
+    });
+
+    //Specs
+    describe('material', function() {
+
+    });
+
+});
---
+++ Pelilogiikka/src/test/javascript/rendering/Scene_spec.js
@@ -0,0 +1,16 @@
+describe('the scene object', function() {
+
+    //Create an easily-removed container for our tests to play in
+    beforeEach(function() {
+    });
+
+    //Clean it up after each spec
+    afterEach(function() {
+    });
+
+    //Specs
+    describe('scene', function() {
+
+    });
+
+});
