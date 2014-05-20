e37e094f416310f8b4b0288be278edff61f2a779
Ubertom <Ubertom@192.168.1.33>
2014-05-21 00:28:35 +0300

    Huomenna lisää... Varsinaisilla XML rutiineilla homma toiminee paremmin. Ikävä kyllä, en vielä oikein hahmota niiden suhdetta dokumentin rakenteeseen.
    Trialilla ja errorilla mennään, mutta lopputulemana XML lataaminen muuttuu yksinkertaiseksi ja vähäriviseksi toimenpiteeksi ja vanha parserinkuvatus voidaan lähettää roskakoriin.

--- Pelilogiikka/src/main/javascript/rendering/parser/GeneralPurposeParser.js
+++ Pelilogiikka/src/main/javascript/rendering/parser/GeneralPurposeParser.js
@@ -1,16 +1,68 @@
 
-/*
-    This is supposed to be a stopgap measure before a proper XML parser can be implemented by someone (else?)
-    It is based on parser_Geometry, but attempts to encapsulate its inner workings and provide a smooth interface
-    for extracting datafields from XML documents
-*/
 
 
+        
+        
+    function DataSheet()
+    {
+    };
+    
+    DataSheet.prototype.push = function( )
+    {
+        
+    }
 
-function GeneralPurposeParser( fileName )
-{
+
     
     
     
     
-}
+    
+    
+    function GeneralPurposeParser( fileName, fetch_Array )
+    {   
+        // First, load the file and store its contents locally as a string.
+        var request = new XMLHttpRequest();
+        request.open("GET", fileName, false);
+        request.send(null);
+        
+        if( request.responseXML == null ) throw "Parsing error - Null response."
+        
+        this.xmlDoc = request.responseXML;
+    }
+    
+            
+
+
+    // -300.000000 -52.500000 -210.000000 0.000000 1.000000 0.000000 ffffffff 0.000000 0.000000
+    //  var data  = xmlDoc.getElementsByTagName("vertices")[0].childNodes[0].nodeValue;
+     
+
+    GeneralPurposeParser.prototype.fetch_Element = function( label, elementSize, initializer ) 
+    {  
+        
+        var data     = this.xmlDoc.getElementsByTagName(label)[0].childNodes[0].nodeValue;
+     
+        console.log( data );
+       
+        
+        /*
+        for( var i = 0; i < tokens.length/elementSize; i += elementSize )
+        {
+            var batch = [];
+            
+            for( var j = 0; j < elementSize-1; j++ )
+            {
+                batch.push( tokens[i+j] );
+            }
+            
+            retData.push( initializer( batch ) );
+          
+      //   alert("Line: " + a + " , " + b + " , " +c );
+        }
+      
+    return retData;
+    */
+    }
+    
+ 
--- Pelilogiikka/src/main/javascript/rendering/parser/parser_Geometry.js
+++ Pelilogiikka/src/main/javascript/rendering/parser/parser_Geometry.js
@@ -113,6 +113,9 @@
     }
 
 
+
+
+
     function get_Batches( rawData )
     {
        var batches     = [];
