9dd1c546a65552ffea9d203dca8e7729cb7fe7ee
Ubertom <Ubertom@192.168.1.33>
2014-05-20 22:31:57 +0300

    Ooouooo! Koodarien kuningas committaa!

---
+++ Pelilogiikka/src/main/javascript/rendering/parser/GeneralPurposeParser.js
@@ -0,0 +1,16 @@
+
+/*
+    This is supposed to be a stopgap measure before a proper XML parser can be implemented by someone (else?)
+    It is based on parser_Geometry, but attempts to encapsulate its inner workings and provide a smooth interface
+    for extracting datafields from XML documents
+*/
+
+
+
+function GeneralPurposeParser( fileName )
+{
+    
+    
+    
+    
+}
