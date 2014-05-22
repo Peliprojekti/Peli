f22838cf8fcd50a06e043b05286d7b8e50d4c3ec
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-05-22 17:20:09 +0300

    tests

---
+++ Pelilogiikka/src/test/javascript/rendering/Renderer_spec.js
@@ -0,0 +1,54 @@
+describe('the renderer object', function() {
+
+    //Create an easily-removed container for our tests to play in
+    beforeEach(function() {
+    });
+
+    //Clean it up after each spec
+    afterEach(function() {
+    });
+
+
+//Testit kommenteissa, koska Renderer vaatii canvasta, joka rikkoo Jasminen.
+
+
+    //Specs
+//    describe('renderer tests', function() {
+//        it('begins', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            testRenderer.begin();
+//            expect(testRenderer.gl).not.toBe(null);
+//        });
+//        it('ends', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            testRenderer.begin();
+//            testRenderer.end();
+//            expect(testRenderer).not.toBe(null);
+//        });
+//        it('draws', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            var testEntity = new Entity(new Mesh, new Material);
+//            testRenderer.draw(testEntity);
+//            expect(testRenderer.gl).not.toBe(null);
+//        });
+//        it('draws more', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            var testEntity = new Entity(new Mesh, new Material);
+//            testRenderer.draw_SS(testEntity);
+//            expect(testRenderer.gl).not.toBe(null);
+//        });
+//        it('binds camera', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            var testCamera = new Camera();
+//            testRenderer.bind_Camera(testCamera);
+//            expect(testRenderer.camera).toEqual(testCamera);
+//        });
+//        it('sets background colors', function() {
+//            var testRenderer = new Renderer(document.createElement('canvas'));
+//            var testColor = new Color(10, 20, 30);
+//            testRenderer.set_BgrColor(testColor);
+//            expect(testRenderer.fillcolor).toEqual(testColor);
+//        });
+//    });
+
+});
