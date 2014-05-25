7926e322020a09233047e1d0c1282de95f0779b2
Hans Liljestrand <ishkamiel@gmail.com>
2014-05-25 20:34:25 +0300

    joo, en vaan osaa

--- Pelilogiikka/src/main/javascript/controller/swipe.js
+++ Pelilogiikka/src/main/javascript/controller/swipe.js
@@ -1,8 +1,6 @@
 var controller = controller || {};
 
 controller.swipe = function(container, canvas, drawText) {
-    var coms = client.coms;
-
     function Swipe() {
         this.updatePeriod = 20; //time in ms
         this.sincePrevious = 0;
@@ -15,16 +13,13 @@ controller.swipe = function(container, canvas, drawText) {
         this.startTime = new Date().getTime();
         this.sincePrevious = 0;
 
-        this.sendCoords();
+        this.sendCoords(event);
     };
 
-    Swipe.prototype.sendCoords = function() {
-        try {
+    Swipe.prototype.sendCoords = function(event) {
         event.preventDefault();
 
-        var coords = [
-            event.changedTouches[0].clientX / canvas.width,
-            event.changedTouches[0].clientY / canvas.height ];
+        var coords = getRelativeCoords(0, event);
 
         if (this.sincePrevious === 0) {
             this.sincePrevious = new Date().getTime() - this.startTime;
@@ -35,27 +30,24 @@ controller.swipe = function(container, canvas, drawText) {
             client.coms.call('swipe', [coords[0], coords[1], this.sincePrevious], null, null);
             log.info("Sent swipe: (" + coords[0] + ", " + coords[1] + ")" + ", " + this.sincePrevious, true);
         }
-        }
-        catch(e) {
-            log.throwToServer(e);
-        }
-    };
 
+    };
 
     Swipe.prototype.doTouchMove = function(event) {
         event.preventDefault();
 
         var currentTime = new Date().getTime();
         if (currentTime - this.previousSendTime >= this.updatePeriod) {
-            this.sendCoords();
+            this.sendCoords(event);
             this.previousSendTime = currentTime;
         }
 
         if (DEBUG) {
-            var coords = getRelativeCoords(0);
+            var coords = getRelativeCoords(0, event);
             updateCoordinatesText(coords[0], coords[1]);
         }
     };
+
 
     Swipe.prototype.enable = function(canvas) {
         var thisObject = this;
@@ -66,6 +58,36 @@ controller.swipe = function(container, canvas, drawText) {
             thisObject.doTouchMove(event);
         }, false);
     };
+
+    function getFingerCoords(id, event) {
+        var canvas_x = event.targetTouches[id].pageX;
+        var canvas_y = event.targetTouches[id].pageY;
+        return [canvas_x, canvas_y];
+    }
+
+    function getRelativeCoords(id, event) {
+        var coords = getFingerCoords(id, event);
+        var canvasDimensions = getCanvasDimensions();
+        var relativeX = coords[0] / canvasDimensions[0];
+        var relativeY = coords[1] / canvasDimensions[1];
+        return [relativeX, relativeY];
+    }
+
+    function getCanvasDimensions() {
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        width = canvas.width;
+        height = canvas.height;
+
+        return [width, height];
+    }
+
+    function updateCoordinatesText(x, y) {
+        var canvasDimensions = getCanvasDimensions();
+
+        drawText("Coordinates: (" + x + ", " + y + ")", 2);
+        drawText("Canvas width: " + canvasDimensions[0] + "\nCanvas height: " + canvasDimensions[1], 3);
+    }
 
     var swipeObj = new Swipe();
     swipeObj.enable(canvas);
