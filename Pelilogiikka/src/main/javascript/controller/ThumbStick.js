7f4d287f93013f9c03ee09af850d3bf114d25ed4
ottis <ottis@ottis-Aspire-V7-582PG.(none)>
2014-05-26 13:31:57 +0300

    piirtoshittiä

--- Pelilogiikka/src/main/javascript/controller/ThumbStick.js
+++ Pelilogiikka/src/main/javascript/controller/ThumbStick.js
@@ -1,29 +1,36 @@
 var controller = controller || {};
 
 controller.thumbStick = function(container, canvas, phone) {
-    function drawCircle(x, y, r) {
-        phone.addDrawable({
-            draw: function() {
-                var context = canvas.getContext("2d");
-
+    var thumbStickDot = {
+                x: 0,
+                y: 0,
+                r: 0,
+                draw: function(context) {
                 context.strokeStyle = "#FF0000";
                 context.fillStyle = "#FFFF00";
                 context.beginPath();
-                context.arc(x, y, r, 0, Math.PI * 2, true);
+                context.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
                 context.closePath();
                 context.stroke();
                 context.fill();
-            }
-        });
+            }};
+        
+    phone.addDrawable(thumbStickDot);
+    
+    function drawCircle(x, y, r) {
+        thumbStickDot.x = x;
+        thumbStickDot.y = y;
+        thumbStickDot.r = r;
     }
 
-    function ThumbStick() {}
+    function ThumbStick() {  }
 
     ThumbStick.prototype.doTouchStart = function(event) {
+        
         event.preventDefault();
         for (var i = 0; i < event.targetTouches.length; i++) {
             if (phone.getRelativeCoords(i, event)[0] < 0.5) {
-                var coords = getFingerCoords(i);
+                var coords = phone.getFingerCoords(i, event);
                 drawCircle(coords[0], coords[1], 10);
             } else {
                 //Napin painallus
@@ -38,7 +45,8 @@ controller.thumbStick = function(container, canvas, phone) {
         event.preventDefault();
         for (var i = 0; i < event.targetTouches.length; i++) {
             if (phone.getRelativeCoords(i, event)[0] < 0.5) {
-                var coords = getFingerCoords(i);
+                
+                var coords = phone.getFingerCoords(i, event);
                 drawCircle(coords[0], coords[1], 10);
             }
         }
