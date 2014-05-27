9ebcff67f60eee2e56aa748d252768bd275d4cc7
ottis <ottis@ottis-Aspire-V7-582PG.(none)>
2014-05-27 14:18:29 +0300

    jotain

---
+++ Pelilogiikka/src/main/javascript/controller/motionController.js
@@ -0,0 +1,106 @@
+var controller = controller || {};
+
+controller.motionController = function(container, canvas, phone) {
+	function MotionController() {
+		//this.coms = null;
+	}
+
+	MotionController.prototype.deviceOrientationHandler = function(eventData) {
+            sendServerMessage("orienatation event");
+		// event.preventDefault();
+
+		// gamma is the left-to-right tilt in degrees, where right is positive
+		var tiltLR = eventData.gamma;
+
+		// beta is the front-to-back tilt in degrees, where front is positive
+		var tiltFB = eventData.beta;
+
+		// alpha is the compass direction the device is facing in degrees
+		var dir = eventData.alpha;
+
+		//  send data to server
+                sendServerMessage("sending");
+		client.coms.call('orientation', [tiltLR, tiltFB, dir], null, null);
+                sendserverMessage("done sending");
+
+		if (DEBUG) {
+			log.info("Orientation changed, new orientation:\n" +
+					"tiltLR (gamma): " + this.toInteger(tiltLR) + "\n" +
+					"tiltFB (beta): " + this.toInteger(tiltFB) + "\n" +
+					"dir (alpha): " + this.toInteger(dir));
+		}
+                sendServerMessage("orientation event done");
+	}
+
+	//motion integration with server not tested yet
+	MotionController.prototype.deviceMotionHandler = function(eventData) {
+		// Grab the acceleration from the results
+		var acceleration = eventData.acceleration;
+		var accelerationIncludingGravity = eventData.accelerationIncludingGravity;
+		var rotation = eventData.rotationRate;
+
+		if (DEBUG) {
+			log.info("Acceleration: \n" +
+					"x: " + acceleration.x + "\n" +
+					"y: " + acceleration.y + "\n" +
+					"z: " + acceleration.z + "\n" +
+					"Acceleration including gravity \n" +
+					"x: " + accelerationIncludingGravity.x + "\n" +
+					"y: " + accelerationIncludingGravity.y + "\n" +
+					"z: " + accelerationIncludingGravity.z + "\n" +
+					"Rotation:\n" +
+					"x: " + rotation.x + "\n" +
+					"y: " + rotation.y + "\n" +
+					"z: " + rotation.z);
+		}
+
+		//lähetä data serverille
+
+	}
+
+	MotionController.prototype.toInteger = function(num) {
+		return parseFloat(num).toFixed(0);
+	}
+
+	MotionController.prototype.enable = function() { //coms, window) {
+            sendServerMessage("enable");
+		var thisObject = this;
+		//this.coms = coms;
+		log.info("Enabling MotionController", true);
+		if (window.DeviceOrientationEvent) {
+			// Listen for the deviceorientation event and handle the raw data
+			window.addEventListener('deviceorientation', function(eventData) {
+					thisObject.deviceOrientationHandler(eventData)
+					}, false);
+
+		} else {
+			log.warn("Device orientation event not supported", true, false);
+		}
+
+/*
+		if (window.DeviceMotionEvent) {
+			window.addEventListener('devicemotion', deviceMotionHandler, false);
+		} else {
+			log.warn("Device motion event not supported");
+		}
+            */
+                sendServerMessage("done enable");
+	}
+
+	MotionController.prototype.disable = function(window) {
+		window.removeEventListener("deviceorientation", this.doTouchMove);
+	}
+
+ sendServerMessage("ok so far");
+    var tdObj = new MotionController();
+    tdObj.enable();
+    sendServerMessage("ok so far, done loading");
+    return function() {
+	// TODO Tähän funktio joka disabloi
+    };
+     
+};
+
+$(document).ready(function() {
+    client.phone.registerController('motionController', controller.motionController);
+});
--- Pelilogiikka/src/main/javascript/game/Controller.js
+++ Pelilogiikka/src/main/javascript/game/Controller.js
@@ -61,7 +61,7 @@ game.Controller.prototype.reset = function() {
     this.tiltFB = null;
     this.dir = null;
     
-    this.speed = 0.1; //proportion of screen height
+    this.speed = 0.1;
 };
 
 game.Controller.prototype.update = function(time) {
@@ -122,16 +122,26 @@ game.Controller.prototype.setControlTypeUpdater = function() {
 
 game.Controller.prototype.setPosition = function(x, y) {
     //console.debug("setPosition ", x, y);
-        if (!(x > 1 || x < 0 || y > 1 || y < 0)) {
+    
         this.x = x;
         this.y = y;
         this.player.setPosition(x, y);
-    }
 };
 
 game.Controller.prototype.pushSwipe = function(x, y, sincePrevious) {
     this.lastSwipe = [[x, y], sincePrevious];
     //log.info("Pushed swipe: (" + x + ", " + y + ")" + ", " + sincePrevious);
+};
+
+game.Controller.prototype.setPositionSwipe = function(x, y) {
+        if (!(x > 1 || x < 0)) {
+            this.x = x;
+        }
+        if (!(y > 1 || y < 0)) {
+            this.y = y;
+        }
+        
+        this.setPosition(this.x, this.y);
 };
 
 
@@ -151,7 +161,7 @@ game.Controller.prototype.calcNewPosition = function(timestamp) {
             this.currentDirection = this.currentDirection.mul(speedMultiplier);    
             var newX = this.x + this.currentDirection.x * this.posChangeMul;
             var newY = this.y + this.currentDirection.y * this.posChangeMul;
-            this.setPosition(
+            this.setPositionSwipe(
                     newX,
                     newY);
             //console.debug("New pos: (" + newX + ", " + newY + ")");
@@ -180,7 +190,7 @@ game.Controller.prototype.calcNewDirection = function(beginning, end) {
         var newX = this.x + newVec.x * this.posChangeMul;
         var newY = this.y + newVec.x * this.posChangeMul;
 
-        this.setPosition(newX, newY);
+        this.setPositionSwipe(newX, newY);
         //log.info("Vector: (" + newVec.x + ", " + newVec.y + ")");
         //console.debug("New position: " + newX + ", " + newY + ")");
     }
@@ -189,17 +199,37 @@ game.Controller.prototype.calcNewDirection = function(beginning, end) {
 };
 
 game.Controller.prototype.orientation = function(tiltLR, tiltFB, dir) {
-    if(dir > 0 && dir < 180){
-        this.setPosition(this.x + speed, this.y);
-        // move right
-    }else{
-        this.setPosition(this.x - speed, this.y);
-        //move left
+    
+    var xPos = 0;
+    
+    if(dir <= 90.0 )
+    {
+       var rads  = (dir*Math.PI)     / 180.0;
+           xPos  = -Math.sin( rads ) / 2.0;
     }
-    log.debug("Orientation changed, new orientation:\n" +
-            "tiltLR (gamma): " + tiltLR + "\n" +
-            "tiltFB (beta): " + tiltFB + "\n" +
-            "dir (alpha): " + dir);
+    else if(dir >= 270.0 )
+    {
+       var rads  = ((360-dir)*Math.PI) / 180.0;
+           xPos  =   Math.sin( rads )  / 2.0;
+    }
+    
+       this.setPosition( 0.5+xPos, this.y);
+    
+//    if(dir < 180){
+//        this.setPosition(this.x - this.speed, this.y);
+//        
+//        // move left
+//    }else{
+//        this.setPosition(this.x + this.speed, this.y);
+//        //move right
+//    }
+    
+    log.debug("pos: (" +this.x + ", " + this.y + ")\n" + 
+            "dir: " + dir);
+//    log.debug("Orientation changed, new orientation:\n" +
+//            "tiltLR (gamma): " + tiltLR + "\n" +
+//            "tiltFB (beta): " + tiltFB + "\n" +
+//            "dir (alpha): " + dir);
 };
 
 game.Controller.prototype.motion = function(accelerationData) {
