c2f71bd6329334e2ff3828768664181da78d882a
Ubertom <Ubertom@192.168.1.33>
2014-05-28 03:27:35 +0300

    Completely overhauling the rendering system. Implementing proper exception handing, strict typing to shader terms, more flexible and encapsulated binding of resources and more.
    After done refactoring the system, will start to implement the spatial partitioning and get the GUI items back up and running. After GUI, will implement sprites to be used as place holders, if proper key-frame animated 3D models become feasible.
    
    Spatial partitioning of the static world mesh is crucial however, if a proper hit detection is to be implemented.

--- Pelilogiikka/src/main/javascript/rendering/Refactored/Resources/Mesh.js
+++ Pelilogiikka/src/main/javascript/rendering/Refactored/Resources/Mesh.js
@@ -1,3 +1,75 @@
+        
+    function Buffer( gl, token ) 
+    {  
+       if( !VALID(token) ) throw new Exception( "NULL", "Undefined Meshbuffer attribute!" );
+        
+       this.gl    = gl;
+       this.data  = gl.createBuffer();		
+       this.label = token.label;
+       
+       if( !this.data )    throw new Exception( "FATAL", "WebGL failed to allocate a buffer" );
+        
+       switch( token.type )
+       {
+           
+           case "VEC3": 
+                            this.item_Size  = 3;
+                            this.item_Type  = gl.FLOAT;
+                            
+                            gl.bindBuffer(gl.ARRAY_BUFFER, this.data );
+                            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array( token.data ), gl.STATIC_DRAW);   
+           break;
+           
+           case "VEC2": 
+                            this.item_Size  = 2;
+                            this.item_Type  = gl.FLOAT;
+                            
+                            gl.bindBuffer(gl.ARRAY_BUFFER, this.data );
+                            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array( token.data ), gl.STATIC_DRAW);
+           break;
+           
+           case "INT" : 
+                            this.item_Size  = 1;
+                            this.itemType   = gl.UNSIGNED_SHORT;
+                            
+                            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.data );
+                            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array( token.data ), gl.STATIC_DRAW);
+                            
+           break;
+           
+           case "FLOAT": 
+                            this.item_Type = gl.FLOAT;
+           break;
+
+       default: throw new Exception( "UNSUPPORTED", "Vertex buffer encountered an unsupported attribute type: " + token.type );
+       }
+        
+    this.data.itemCount = token.data.length / this.data.itemSize;     
+    }   
+    
+    
+    Buffer.prototype.bind = function( target ) 
+    {
+        this.gl.bindBuffer( this.gl.ARRAY_BUFFER, this.data );
+        this.gl.vertexAttribPointer( target, this.item_Size, this.item_Type, false, 0, 0);   
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function VertexAttribute( type, label, data )
 {
