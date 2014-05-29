7132bcc5e9af63d5f37533fd18d356e45602d667
Hans Liljestrand <ishkamiel@gmail.com>
2014-05-29 17:46:26 +0300

    cleaning up

--- Pelilogiikka/src/main/javascript/game/dummy/dummy.js
+++ Pelilogiikka/src/main/javascript/game/dummy/dummy.js
@@ -1,5 +1,10 @@
-var dummy = dummy || {};
+/*global console: false*/
+/*global $: false*/
+/*global game: false*/
+/*global graphics2d: false*/
+/*jslint browser: true*/
 
+/*
 var MESSAGE_TIME = 10000;
 
 var canvas = null;
@@ -14,151 +19,114 @@ var message_timer = [];
 var context = null;
 
 var updateables = [];
+*/
 
-function showMessage(msg) {
-	log.info("displaying message: " + msg);
-	messages.push(msg);
-	message_timer.push(MESSAGE_TIME);
-}
+var dummy = {
+    start: function () {
+        "use strict";
+        dummy.screen.load(document.getElementById("canvas"));
 
-function initializeUI() {
-	canvas = setupCanvas();
+        //this.updateables.push(graphics2d.fpsDisplay.createFancy(canvas));
+        //this.updateables.push(graphics2d.rpsDisplay.createFancy(canvas));
+        //this.updateables.push(graphics2d.playerPerformance.create(canvas));
 
-	//context = canvas.getContext('2d');
+        this.connectToServer();
 
-	c_width = canvas.width;
-	c_height = canvas.height;
-
-	this.updateables.push(graphics2d.fpsDisplay.createFancy(canvas));
-	this.updateables.push(graphics2d.rpsDisplay.createFancy(canvas));
-	//this.updateables.push(graphics2d.playerPerformance.create(canvas));
-
-	requestAnimationFrame(animate);
-
-	showMessage("starting anmiations");
-}
-
-function setupCanvas() {
-	var container = document.getElementById("container");
-	canvas = document.getElementById("canvas");
-
-	var width = container.clientWidth;
-	var height = container.clientHeight;
-
-	c_height = height;
-	c_width = width;
-
-	log.info("setting canvas dimensions to " + c_width + "x" + c_height);
-	canvas.width = c_width;
-	canvas.height = c_height;
-
-	return canvas;
-}
-
-
-
-function connectToServer() {
-	var players = this.players;
-
-	log.info("Opening controllerHub");
-
-	/*
-	 game.controllerHub.addCustomRpcMethod(
-	 'playerPerformanceReport',
-	 graphics2d.playerPerformance,
-	 graphics2d.playerPerformance.playerPerformanceReport);
-	 */
-
-	game.controllerHub.openHub(
-		function(player) { // onPlayerJoined
-			log.info("New player connected to dummy game");
-			var crosshair = new graphics2d.crosshair.createRandomColor(0.5, 0.5, 20);//Crosshair(0, 0, 20);
-			player.setCrosshair(crosshair);
-			players.push(player);
-			//playerPerformance.addPlayer(player);
-		},
-		function(player) { // onPlayerLeft
-			//playerPerformance.removePlayer(player);
-			var i = 0;
-			for (; i < players.length; i++) {
-				if (players[i] === player) {
-					break;
-				}
-			}
-			players.splice(i, 1);
-		},
-		100 // maxPlayers
-		);
-
-	//controllers.open();
-}
-
-function animate(time) {
-	update(time);
-	draw(time);
-
-	requestAnimationFrame(animate);
-}
-
-function update(time) {
-	players.forEach(function(player) {
-		player.update(time);
-	});
-
-	updateables.forEach(function(updatable) {
-		updatable.update(time);
-	});
-}
-
-function draw(time) {
-	ctx = canvas.getContext('2d');
-	ctx.clearRect(0, 0, c_width, c_height);
-
-	drawRectangle(ctx);
-
-	updateables.forEach(function(updatable) {
-		updatable.draw(ctx, time);
-	});
-
-
-	players.forEach(function(player) {
-		player.draw(ctx, time);
-	});
-
-	ctx.restore();
-}
-
-var testRect = {
-	x: 0,
-	y: 0,
-	width: 100,
-	height: 100,
-	xm: 1,
-	ym: 1
+        //window.requestAnimationFrame(animate);
+    },
+    onPlayerJoined: function (player) {
+        "use strict";
+        console.info("New player connected ", player);
+        var crosshair = graphics2d.crosshair.createRandomColor(0.5, 0.5, 20);//Crosshair(0, 0, 20);
+        player.setCrosshair(crosshair);
+        dummy.players.push(player);
+    },
+    onPlayerLeft: function (player) {
+        "use strict";
+        console.info("Player disconnected ", player);
+        //playerPerformance.removePlayer(player);
+        var i = 0;
+        while (i < dummy.players.length) {
+            if (dummy.players[i] === player) {
+                break;
+            }
+            i += 1;
+        }
+        dummy.players.splice(i, 1);
+    },
+    connectToServer: function () {
+        "use strict";
+        game.controllerHub.openHub(dummy.onPlayerJoined, dummy.onPlayerLeft, 100);
+    }
 };
 
-function drawRectangle(ctx) {
-	ctx.save();
-	ctx.fillStyle = "rgb(200, 0, 0)";
-	testRect.x += testRect.xm;
-	testRect.y += testRect.ym;
-	ctx.fillRect(testRect.x, testRect.y, 100, 100);
-	if (testRect.x <= 0) {
-		testRect.xm = 1;
-	} else if (testRect.x >= (c_width - testRect.width)) {
-		testRect.xm = -1;
-	}
-	if (testRect.y <= 0) {
-		testRect.ym = 1;
-	} else if (testRect.y >= (c_height - testRect.height)) {
-		testRect.ym = -1;
-	}
-	ctx.restore();
-}
-
-$(document).ready(function() {
-	log.info("initializing dummy UI");
-	initializeUI();
-	log.info("connecting dummy to server");
-	connectToServer();
+$(document).ready(function () {
+    "use strict";
+    dummy.start();
 });
+
+/*
+ function animate (time) {
+ update(time);
+ draw(time);
+ 
+ requestAnimationFrame(animate);
+ }
+ 
+ function update (time) {
+ players.forEach(function (player) {
+ player.update(time);
+ });
+ 
+ updateables.forEach(function (updatable) {
+ updatable.update(time);
+ });
+ }
+ 
+ function draw (time) {
+ ctx = canvas.getContext('2d');
+ ctx.clearRect(0, 0, c_width, c_height);
+ 
+ drawRectangle(ctx);
+ 
+ updateables.forEach(function (updatable) {
+ updatable.draw(ctx, time);
+ });
+ 
+ 
+ players.forEach(function (player) {
+ player.draw(ctx, time);
+ });
+ 
+ ctx.restore();
+ }
+ 
+ var testRect = {
+ x: 0,
+ y: 0,
+ width: 100,
+ height: 100,
+ xm: 1,
+ ym: 1
+ };
+ 
+ function drawRectangle (ctx) {
+ ctx.save();
+ ctx.fillStyle = "rgb(200, 0, 0)";
+ testRect.x += testRect.xm;
+ testRect.y += testRect.ym;
+ ctx.fillRect(testRect.x, testRect.y, 100, 100);
+ if (testRect.x <= 0) {
+ testRect.xm = 1;
+ } else if (testRect.x >= (c_width - testRect.width)) {
+ testRect.xm = -1;
+ }
+ if (testRect.y <= 0) {
+ testRect.ym = 1;
+ } else if (testRect.y >= (c_height - testRect.height)) {
+ testRect.ym = -1;
+ }
+ ctx.restore();
+ }
+ */
+
--- Pelilogiikka/src/main/javascript/game/dummy/gameScreen.js
+++ Pelilogiikka/src/main/javascript/game/dummy/gameScreen.js
@@ -1,7 +1,22 @@
-var dummy = dummy || {};
+/*global dummy: false*/
+/*global console: false*/
+/*jslint browser: true*/
 
 dummy.screen = {
-	load: function(canvas) {
-	}
+    canvas: null,
+    load: function (canvas) {
+        "use strict";
+        this.canvas = canvas;
 
+    },
+    p_setupCanvas: function () {
+        "use strict";
+        var container = document.getElementById("container");
+        this.canvas = document.getElementById("canvas");
+
+        this.canvas.width = container.clientWidth;
+        this.canvas.height = container.clientHeight;
+
+        console.info("Canvas dimension is ", this.width, this.height);
+    }
 };
