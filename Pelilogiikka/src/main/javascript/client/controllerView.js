a742582bf33b3e8952ac57b23e197a563621a80d
Hans Liljestrand <ishkamiel@gmail.com>
2014-05-31 18:15:26 +0300

    tunetusta ja linttausta

--- Pelilogiikka/src/main/javascript/client/controllerView.js
+++ Pelilogiikka/src/main/javascript/client/controllerView.js
@@ -1,57 +1,101 @@
+/*jslint browser: true*/
+/*global console: false*/
+/*global client: false*/
 var client = client || {};
 
 client.controllerView = {
+    stopped: false,
     debug: false,
     entities: [],
+    crosshair: null,
     container: null,
+    context: null,
+    width: 0,
+    height: 0,
     canvas: null,
-
-    create: function(container, canvas) {
+    create: function (container, canvas) {
+        "use strict";
         this.container = container;
         this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+        this.width = canvas.width;
+        this.height = canvas.height;
         return this;
     },
+    start: function () {
+        "use strict";
+        var self = this;
+        this.stopped = false;
 
-    onResize: function() {
-        this.canvas.width = container.offsetWidth;
-        this.canvas.height = container.offsetHeight;
-        //$("#canvas").attr('width', $("#container").width());
-        //$("#canvas").attr('height', $("#container").height());
-        
-        this.entities.forEach(function(e) {
-            if (e.onResize) e.onResize();
+        function animate (time) {
+            self.update(time);
+            self.draw(time);
+
+            if (!self.stopped) {
+                window.requestAnimationFrame(animate);
+            }
+        }
+
+        window.requestAnimationFrame(animate);
+    },
+    stop: function () {
+        "use strict";
+        this.stopped = true;
+    },
+    onResize: function () {
+        "use strict";
+        this.width = this.canvas.width = this.container.offsetWidth;
+        this.height = this.canvas.height = this.container.offsetHeight;
+
+        this.entities.forEach(function (e) {
+            if (e.onResize)
+                e.onResize();
         });
     },
-
-    onOrientationChange: function() {
-        this.entities.forEach(function(e) {
-            if (e.onOrientationChange) e.onOrienationChange();
+    onOrientationChange: function () {
+        "use strict";
+        this.entities.forEach(function (e) {
+            if (e.onOrientationChange)
+                e.onOrienationChange();
         });
     },
-
-    update: function(time) {
-        this.entities.forEach(function(e) {
-            if (e.update) e.update(time);
+    update: function (time) {
+        "use strict";
+        this.entities.forEach(function (e) {
+            if (e.update)
+                e.update(time);
         });
     },
-
-    draw: function(time) {
-        var ctx = this.canvas.getContext('2d');
-        var width = self.canvas.width;
-        var height = self.canvas.height;
+    draw: function () {
+        "use strict";
+        var ctx = this.context;
+        ctx.save();
 
         // fill background
         ctx.fillStyle = 'green';
-        ctx.fillRect(0, 0, width, height);
+        ctx.fillRect(0, 0, this.width, this.height);
         ctx.fillStyle = 'black';
-        ctx.fillRect(10, 10, width - 20, height - 20);
+        ctx.fillRect(10, 10, this.width - 20, this.height - 20);
 
-        this.entities.forEach(function(e) {
-            if (e.draw) e.draw(ctx, time);
+        if (this.crosshair !== null) 
+            ctx.drawImage(this.crosshair, this.width/2 - 32, this.height/2 - 32);
+
+        this.entities.forEach(function (e) {
+            if (e.draw) {
+                e.draw(ctx);
+            }
         });
-    },
 
-    add: function(object) {
+        ctx.restore();
+    },
+    add: function (object) {
+        "use strict";
         this.entities.push(object);
+    },
+    showCrosshair: function(id) {
+        if (id !== null) {
+            this.crosshair = new Image();
+            this.crosshair.src = "/data/crosshairs/crosshair" + id + ".png";
+        }
     }
 };
