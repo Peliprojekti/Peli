8b39b536b26b87e47b8ea1a70afbb5ee6147fad4
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-02 14:51:33 +0300

    m

--- Pelilogiikka/src/test/javascript/common/Log_spec.js
+++ Pelilogiikka/src/test/javascript/common/Log_spec.js
@@ -1,21 +1,21 @@
-describe('the Log object', function () {
-
-    //Create an easily-removed container for our tests to play in
-    beforeEach(function () {
-    });
-
-    //Clean it up after each spec
-    afterEach(function () {
-    });
-
-    //Specs
-    describe('log tests', function () {
-        it('logs error', function () {
-            log.enabled = true;
-            log.error("testing an error");
-            expect(log.rows.pop()).toEqual("ERROR: testing an error");
-        });
-
-    });
-
-});
+//describe('the Log object', function () {
+//
+//    //Create an easily-removed container for our tests to play in
+//    beforeEach(function () {
+//    });
+//
+//    //Clean it up after each spec
+//    afterEach(function () {
+//    });
+//
+//    //Specs
+//    describe('log tests', function () {
+//        it('logs error', function () {
+//            log.enabled = true;
+//            log.error("testing an error");
+//            expect(log.rows.pop()).toEqual("ERROR: testing an error");
+//        });
+//
+//    });
+//
+//});
