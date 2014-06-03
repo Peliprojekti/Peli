db93638cef69d2bdb6de7920c02f4637d15bff55
pete <pcmakine@da1-cs-b221-19.helsinki.fi>
2014-06-03 12:59:23 +0300

    thumbstick tests

--- Pelilogiikka/src/test/javascript/controller/ThumbStick_spec.js
+++ Pelilogiikka/src/test/javascript/controller/ThumbStick_spec.js
@@ -11,26 +11,31 @@ describe('the ThumbStick object', function () {
     //Specs
     describe('ThumbStick tests', function () {
 
-        it('initializes the canvas', function () {
+        it('calls coms with the correct method and position parameters on touch start', function () {
             var canvas = document.createElement("canvas");
+            canvas.width = 100;
+            canvas.height = 100;
             var coms = {
                 call: function (method, params, object, callback) {
                 }
             };
 
             spyOn(coms, 'call');
-
             client.loadedTypes.ThumbStick(null, canvas, null, coms);
-
             var evt = document.createEvent("Events")
             //Aim: initialize it to be the event we want
             evt.initEvent('touchstart', true, true); //true for can bubble, true for cancelable
             evt.targetTouches = [{pageX: 12, pageY: 10}, {pageX: 12, pageY: 10}];
-            //FIRE!
             canvas.dispatchEvent(evt);
-            
+
+            var arr = [(12 / canvas.width) * 2, (10 / canvas.height) * 2];
+
             expect(coms.call).toHaveBeenCalled();
+            expect(coms.call).toHaveBeenCalledWith('thumbStickPosition', arr, null, null);
+
         });
+
+
 
     });
 
