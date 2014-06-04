58dcfe816d6d1551d60d21ad8b2896f8dbed7250
Hantula Otto A <ottohant@da1-cs-b221-20.helsinki.fi>
2014-06-04 12:17:18 +0300

    viivoja menee pystysuunnassa

--- Pelilogiikka/src/main/javascript/game/dummy/statsDisplay.js
+++ Pelilogiikka/src/main/javascript/game/dummy/statsDisplay.js
@@ -18,9 +18,14 @@ dummy.statsDisplay = {
         this.fpsDisplay = fpsDisplay.createFancy(canvas);
         this.rpsDisplay = rpsDisplay.createFancy(canvas);
         this.y = Math.floor(canvas.height / 8);
+        this.xStart = Math.floor(canvas.width / 8);
+        this.yStart = this.y;
         this.canvasWidth = canvas.width;
+        this.canvasHeight = canvas.height;
         this.width = Math.floor(canvas.width / 10);
-        this.delta = Math.floor(canvas.width / 150);
+        this.deltaX = Math.floor(canvas.width / 150);
+        this.deltaY = Math.floor(canvas.height / 150);
+        this.verticalLineCount = 3;
 
         return this;
     },
@@ -37,42 +42,55 @@ dummy.statsDisplay = {
         ctx.save();
         this.fpsDisplay.draw(ctx);
         this.rpsDisplay.draw(ctx);
-        
+
         ctx.strokeStyle = "#000000";
         ctx.lineWidth = 3;
 
         for (var i = 1; i <= 7; i++) {
             ctx.beginPath();
-            ctx.moveTo(this.x, this.y*i);
-            ctx.lineTo(this.x + (this.width), this.y*i);
+            ctx.moveTo(this.x, this.yStart * i);
+            ctx.lineTo(this.x + (this.width), this.yStart * i);
             ctx.closePath();
             ctx.stroke();
         }
 
-        this.x += this.delta;
-        if (this.x <= 0 || (this.x  + this.width) >= this.canvasWidth) {
-            this.delta *= -1;
+        for (var i = 1; i <= this.verticalLineCount; i++) {
+            ctx.beginPath();
+            ctx.moveTo(this.xStart, this.y + (this.yStart * i));
+            ctx.lineTo(this.xStart + (this.width), this.y + (this.yStart * i));
+            ctx.closePath();
+            ctx.stroke();
         }
- 
+
+        this.x += this.deltaX;
+        this.y += this.deltaY;
+        if (this.x <= 0 || (this.x + this.width) >= this.canvasWidth) {
+            this.deltaX *= -1;
+        }
+
+        if (this.y <= 0 || (this.y + (this.yStart*this.verticalLineCount) ) >= this.canvasHeight) {
+            this.deltaY *= -1;
+        }
+
         ctx.fillStyle = '#3030BB';
         ctx.strokeStyle = '#202090';
         ctx.lineWidth = 1;
 
         ctx.font = 'bold 40pt Calibri';
         ctx.textAlign = 'center';
-        ctx.fillText(this.openConnections, this.canvasWidth/2, 70);
+        ctx.fillText(this.openConnections, this.canvasWidth / 2, 70);
         //ctx.strokeText(this.fps, this.x, this.y);
         ctx.font = 'normal 12pt Calibri';
-        ctx.fillText('open connections', this.canvasWidth/2, 20);
-        ctx.strokeText('open connections', this.canvasWidth/2, 20);
+        ctx.fillText('open connections', this.canvasWidth / 2, 20);
+        ctx.strokeText('open connections', this.canvasWidth / 2, 20);
 
         ctx.font = 'bold 40pt Calibri';
         ctx.textAlign = 'center';
-        ctx.fillText(this.connectedPlayers, this.canvasWidth/2, 150);
+        ctx.fillText(this.connectedPlayers, this.canvasWidth / 2, 150);
         //ctx.strokeText(this.fps, this.x, this.y);
         ctx.font = 'normal 12pt Calibri';
-        ctx.fillText('connected players', this.canvasWidth/2, 100);
-        ctx.strokeText('connected players', this.canvasWidth/2, 100);
+        ctx.fillText('connected players', this.canvasWidth / 2, 100);
+        ctx.strokeText('connected players', this.canvasWidth / 2, 100);
         ctx.restore();
     }
 };
