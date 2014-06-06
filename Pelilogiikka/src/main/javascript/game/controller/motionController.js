3ac373e5a034e7b000765b2437159133252acb9c
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-06 16:11:24 +0300

    orientcontrol

--- Pelilogiikka/src/main/javascript/game/controller/motionController.js
+++ Pelilogiikka/src/main/javascript/game/controller/motionController.js
@@ -3,53 +3,79 @@ var controller = controller || {};
 controller.loadedTypes = controller.loadedTypes || [];
 controller.loadedTypes['motionController'] = {
     freeControllers: [],
-
-    getController: function(player, rpc) {
+    getController: function (player, rpc) {
         // jos on vapaita niin antaa sellasen, muuten luo uuden
         return (this.freeControllers.length > 0 ?
-                this.freeControllers.pop().reset(player, rpc) :
-                new controller.MotionController(player, rpc)
-               );
+            this.freeControllers.pop().reset(player, rpc) :
+            new controller.MotionController(player, rpc)
+            );
     },
-
-    freeController: function(controller) {
+    freeController: function (controller) {
         //controller.motionController.freeControllers.push(controller);
     }
 };
 
-controller.MotionController = function(player, rpc) {
+controller.MotionController = function (player, rpc) {
     this.reset(player, rpc);
 };
 
-controller.MotionController.prototype.clear = function() {
+controller.MotionController.prototype.clear = function () {
     this.player = null;
 };
 
-controller.MotionController.prototype.reset = function(player, rpc) {
+controller.MotionController.prototype.reset = function (player, rpc) {
     this.player = player;
     this.x = player.x;
     this.y = player.y;
+    this.tiltLR = 0;
+    this.tiltFB = 0;
+    this.multiplier = 0.01;
     rpc.exposeRpcMethod('orientation', this, this.orientation);
     rpc.exposeRpcMethod('motion', this, this.motion);
 };
 
-controller.MotionController.prototype.update = function() {};
-
-controller.MotionController.prototype.position = function(x,y) {
-    this.player.setPosition(x,y);
+controller.MotionController.prototype.update = function () {
+    
+    this.setPosition(this.x + this.tiltFB * this.multiplier, this.y + this.tiltFB * this.multiplier);
 };
 
-controller.MotionController.prototype.setPosition = function(x, y) {
+controller.MotionController.prototype.position = function (x, y) {
+    this.player.setPosition(x, y);
+};
+
+controller.MotionController.prototype.setPosition = function (x, y) {
     //console.debug("setPosition ", x, y);
     this.x = x;
     this.y = y;
     this.player.setPosition(x, y);
 };
 
-controller.MotionController.prototype.orientation = function(tiltLR, tiltFB, dir) {
+controller.MotionController.prototype.orientation = function (tiltLR, tiltFB, dir) {
+    this.tiltLR = tiltLR;
+    this.tiltFB = tiltFB;
+
+
+
+//    if(dir < 180){
+//        this.setPosition(this.x - this.speed, this.y);
+//        
+//        // move left
+//    }else{
+//        this.setPosition(this.x + this.speed, this.y);
+//        //move right
+//    }
+
+    log.debug("pos: (" + this.x + ", " + this.y + ")\n" +
+        "dir: " + dir);
+//    log.debug("Orientation changed, new orientation:\n" +
+//            "tiltLR (gamma): " + tiltLR + "\n" +
+//            "tiltFB (beta): " + tiltFB + "\n" +
+//            "dir (alpha): " + dir);
+};
+
+controller.MotionController.prototype.direction = function (tiltLR, tiltFB, dir) {
     var xPos = 0;
     var rads;
-
     if (dir <= 90.0)
     {
         rads = (dir * Math.PI) / 180.0;
@@ -62,39 +88,23 @@ controller.MotionController.prototype.orientation = function(tiltLR, tiltFB, dir
     }
 
     this.setPosition(0.5 + xPos, this.y);
+}
 
-//    if(dir < 180){
-//        this.setPosition(this.x - this.speed, this.y);
-//        
-//        // move left
-//    }else{
-//        this.setPosition(this.x + this.speed, this.y);
-//        //move right
-//    }
-
-    log.debug("pos: (" + this.x + ", " + this.y + ")\n" +
-            "dir: " + dir);
-//    log.debug("Orientation changed, new orientation:\n" +
-//            "tiltLR (gamma): " + tiltLR + "\n" +
-//            "tiltFB (beta): " + tiltFB + "\n" +
-//            "dir (alpha): " + dir);
-};
-
-controller.MotionController.prototype.motion = function(accelerationData) {
+controller.MotionController.prototype.motion = function (accelerationData) {
     var acceleration = accelerationData.acceleration;
     var accelerationIncludingGravity = accelerationData.accelerationIncludingGravity;
     var rotation = accelerationData.rotationRate;
     log.info("Acceleration: \n" +
-            "x: " + acceleration.x + "\n" +
-            "y: " + acceleration.y + "\n" +
-            "z: " + acceleration.z + "\n" +
-            "Acceleration including gravity \n" +
-            "x: " + accelerationIncludingGravity.x + "\n" +
-            "y: " + accelerationIncludingGravity.y + "\n" +
-            "z: " + accelerationIncludingGravity.z + "\n" +
-            "Rotation:\n" +
-            "x: " + rotation.x + "\n" +
-            "y: " + rotation.y + "\n" +
-            "z: " + rotation.z);
+        "x: " + acceleration.x + "\n" +
+        "y: " + acceleration.y + "\n" +
+        "z: " + acceleration.z + "\n" +
+        "Acceleration including gravity \n" +
+        "x: " + accelerationIncludingGravity.x + "\n" +
+        "y: " + accelerationIncludingGravity.y + "\n" +
+        "z: " + accelerationIncludingGravity.z + "\n" +
+        "Rotation:\n" +
+        "x: " + rotation.x + "\n" +
+        "y: " + rotation.y + "\n" +
+        "z: " + rotation.z);
 };
 
