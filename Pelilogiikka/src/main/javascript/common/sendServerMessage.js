28dec45f43a134f878023b3e72fd129765519375
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-09 13:30:52 +0300

    m

--- Pelilogiikka/src/main/javascript/common/sendServerMessage.js
+++ Pelilogiikka/src/main/javascript/common/sendServerMessage.js
@@ -1,25 +1,27 @@
 /**
  * Sends messages to server
  */
-sendServerMessage = function() {
-    if (sendServerMessage._socket === undefined || sendServerMessage._socket === null) {
-        var socket = eio.Socket(
-                { host: location.hostname, port: 1340 }, // TODO hardcoded port here!
-                { transports: ['websocket','polling'] });
+sendServerMessage = function () {
+    if (eio !== null){
+        if (sendServerMessage._socket === undefined || sendServerMessage._socket === null) {
+            var socket = eio.Socket(
+                {host: location.hostname, port: 1340}, // TODO hardcoded port here!
+            {transports: ['websocket', 'polling']});
 
-        socket.on('close', function() {
-            console.warn("sendServerMessage disconnected");
-            sendServerMessage._socket = null;
-        });
+            socket.on('close', function () {
+                console.warn("sendServerMessage disconnected");
+                sendServerMessage._socket = null;
+            });
 
-        socket.on('error', function() {
-            console.errorr("sendServerMessage connection error");
-            sendServerMessage._socket = null;
-        });
+            socket.on('error', function () {
+                console.errorr("sendServerMessage connection error");
+                sendServerMessage._socket = null;
+            });
 
-        sendServerMessage._socket = socket;
+            sendServerMessage._socket = socket;
+        }
+
+        console.info("serverMessage: ", arguments);
+        sendServerMessage._socket.send(JSON.stringify(arguments));
     }
-
-    console.info("serverMessage: ", arguments);
-    sendServerMessage._socket.send(JSON.stringify(arguments));
 };
