c67797a9aba67fed82ac395bc6ebbd59409b4581
ishkamiel <ishkamiel@gmail.com>
2014-06-09 15:10:29 +0300

    testejä

--- Pelilogiikka/src/test/javascript/common/ConnectionEngineIO_spec.js
+++ Pelilogiikka/src/test/javascript/common/ConnectionEngineIO_spec.js
@@ -1,35 +1,192 @@
-/*
-describe('the ConnectionEngineIO object', function() {
+/*global ConnectionEngineIO: true*/
+/*global console: false*/
+/*global describe: false */
+/*global expect: false */
+/*global it: false */
+/*global spyOn: false */
 
-    //Create an easily-removed container for our tests to play in
-    beforeEach(function() {
+describe('the ConnectionEngineIO object', function () {
+    "use strict";
+
+    var doNothing = function () {
+        console.log("doing nothing");
+    };
+
+    function getSimpleCallbackObject() {
+        return {
+            lastSent: null,
+            lastOpen: {
+                error: null,
+                ok: null
+            },
+            open: function (error, ok) {
+                this.lastOpen.error = error;
+                this.lastOpen.ok = ok;
+            },
+            close: function () {
+                console.log("doing nothing");
+            },
+            onMessage: function (msg) {
+                this.lastSent = msg;
+            }
+        };
+    }
+
+    ConnectionEngineIO.eio = {
+        Socket: function () {
+            return new ConnectionEngineIO.eio.SocketObject();
+        },
+        SocketObject: function () {
+            this.readyState = 'closed'; // not from spec...
+            this.lastSent = null;
+            this.listeners = {};
+        }
+    };
+
+    ConnectionEngineIO.eio.SocketObject.prototype.send = function (msg) {
+        this.lastSent = msg;
+    };
+
+    ConnectionEngineIO.eio.SocketObject.prototype.on = function (event, listener) {
+        this.listeners[event] = listener;
+    };
+
+    ConnectionEngineIO.eio.SocketObject.prototype.close = function () {
+        console.log("closing connection");
+    };
+
+    ConnectionEngineIO.eio.SocketObject.prototype.launchEvent = function (event, args) {
+        this.listeners[event](args);
+    };
+
+    describe('constructor', function () {
+        it('works', function () {
+            var connection;
+
+            expect(function () {
+                connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
+            }).not.toThrow();
+
+            expect(connection.isOpen()).toBe(false);
+        });
     });
 
-    //Clean it up after each spec
-    afterEach(function() {
+    describe('connect', function () {
+        it('works', function () {
+            var connection;
+
+            connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
+
+            expect(function () {
+                connection.connect(doNothing, doNothing, doNothing);
+            }).not.toThrow();
+        });
+
+        it('catches missing onMessage callback', function () {
+            var connection;
+
+            connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
+
+            expect(function () {
+                connection.connect(doNothing, doNothing, null);
+            }).toThrow();
+        });
     });
-    
-//    Testit kommenteissa, koska luokka ConnectionEngineIO vaatii toimiakseen luokkia, jotka rikkovat testiympäristön.
 
-    //Specs
-//    describe('ConnectionEngineIO tests', function() {
-//        it('opens a connection', function() {
-//            var testCEIO = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
-//            expect(testCEIO.connect()).toBeTruthy();
-//        });
-//        it('recognizes the open connection correctly', function() {
-//            var testCEIO = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
-//            expect(testCEIO.isOpen()).toBeTruthy();
-//        });
-//        it('closes the connection', function() {
-//            var testCEIO = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
-//            expect(testCEIO.close()).toBeTruthy();
-//        });
-//        it('sends a message', function() {
-//            var testCEIO = new ConnectionEngineIO("http://localhost", 8000, "protocol", true);
-//            expect(testCEIO.sendMessage()).toBeTruthy();
-//        });
-//    });
+    describe('socket connect/close events events', function () {
+        it('recognizes opened connection', function () {
+            var connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true),
+                cb = getSimpleCallbackObject(),
+                socket;
 
+            spyOn(cb, 'close').andCallThrough();
+            spyOn(cb, 'open').andCallThrough();
+
+            expect(connection.isOpen()).toBe(false);
+            connection.connect(
+                function (er, ok) { cb.open(er, ok); },
+                function (er, ok) { cb.close(er, ok); },
+                function (msg) { cb.onMessage(msg); }
+            );
+
+            socket = connection.getSocket();
+            socket.launchEvent('open');
+
+            expect(cb.lastOpen.error).toBe(null);
+            expect(cb.lastOpen.ok).toBe(true);
+            expect(connection.isOpen()).toBe(true);
+            expect(cb.open).toHaveBeenCalled();
+            expect(cb.close).not.toHaveBeenCalled();
+
+            socket.launchEvent('close');
+            expect(cb.close).toHaveBeenCalled();
+            expect(connection.isOpen()).toBe(false);
+        });
+    });
+
+    describe('socket connect/close events events', function () {
+        it('recognizes opened connection', function () {
+            var connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true),
+                cb = getSimpleCallbackObject(),
+                socket;
+
+            expect(connection.isOpen()).toBe(false);
+            connection.connect(
+                function (er, ok) { cb.open(er, ok); },
+                function (er, ok) { cb.close(er, ok); },
+                function (msg) { cb.onMessage(msg); }
+            );
+
+            socket = connection.getSocket();
+            socket.launchEvent('open');
+
+            socket.launchEvent('error', "virhe");
+
+            expect(cb.lastOpen.error).toBe('virhe');
+            expect(cb.lastOpen.ok).toBe(null);
+            expect(connection.isOpen()).toBe(false);
+        });
+    });
+
+    describe('socket error events', function () {
+        it('properly handes connection errors', function () {
+            var connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true),
+                cb = getSimpleCallbackObject(),
+                socket;
+
+            expect(connection.isOpen()).toBe(false);
+            connection.connect(
+                function (er, ok) { cb.open(er, ok); },
+                function (er, ok) { cb.close(er, ok); },
+                function (msg) { cb.onMessage(msg); }
+            );
+
+            socket = connection.getSocket();
+            socket.launchEvent('open');
+
+            socket.launchEvent('message', "kiva viesti");
+            expect(cb.lastSent).toBe("kiva viesti");
+        });
+    });
+
+    describe('socket connect/close events events', function () {
+        it('recognizes opened connection', function () {
+            var connection = new ConnectionEngineIO("http://localhost", 8000, "protocol", true),
+                cb = getSimpleCallbackObject(),
+                socket;
+
+            expect(connection.isOpen()).toBe(false);
+            connection.connect(
+                function (er, ok) { cb.open(er, ok); },
+                function (er, ok) { cb.close(er, ok); },
+                function (msg) { cb.onMessage(msg); }
+            );
+
+            socket = connection.getSocket();
+            socket.launchEvent('open');
+
+            connection.sendMessage('joku viesti');
+            expect(socket.lastSent).toBe('joku viesti');
+        });
+    });
 });
-*/
