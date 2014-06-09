ac548a70191e09895be9851f065ab1246476c9ca
ishkamiel <ishkamiel@gmail.com>
2014-06-09 17:28:33 +0300

    log.js test coverage 100%

--- Pelilogiikka/src/main/javascript/common/log.js
+++ Pelilogiikka/src/main/javascript/common/log.js
@@ -1,61 +1,73 @@
 var log = {
     enabled: DEBUG,
     level: 3,
-    rows: [],
-    //coms: null,
     serverMsgr: null,
     
-    logMessage: function(type, msg, send, benchmark) {
+    logMessage: function(type, msg, send) {
         var message = type + msg;
         console.log(message);
-        
-        if (benchmark) {
-            log.rows.push(message);
-        }
         
         if (send) {
             sendServerMessage(message);
         }
     },
 
-    error: function(msg, send, benchmark) {
+    error: function(msg, send) {
         if(log.enabled) {
-            log.logMessage("ERROR: ", msg, send, benchmark);
+            log.logMessage("ERROR: ", msg, send );
         }
     },
 
-    warn: function(msg, send, benchmark) {
+    warn: function(msg, send) {
         if(log.enabled && log.level > 0) {
-            log.logMessage("WARN: ", msg, send, benchmark);
+            log.logMessage("WARN: ", msg, send);
         }
     },
 
-    info: function(msg, send, benchmark) {
+    info: function(msg, send) {
         if(log.enabled && log.level > 1) {
-            log.logMessage("INFO: ", msg, send, benchmark);
+            log.logMessage("INFO: ", msg, send);
         }
     },
 
-    debug: function(msg, send, benchmark) {
+    debug: function(msg, send) {
         if(log.enabled && log.level > 2) {
-            log.logMessage("DEBUG: ", msg, send, benchmark);
+            log.logMessage("DEBUG: ", msg, send);
         }
     },
 
-    set_level: function(level) {
+    setLevel: function(level) {
         log.level = level;
+    },
+
+    getLevel: function() {
+        return log.level;
     },
 
     throwToServer: function(error) {
         sendServerMessage(error);
     },
+
+    resetDebug: function(debug) {
+        if (!debug) {
+            console.info("disabling all console log/info/debug messages");
+            log.enbaled = false;
+            log.consInfo = console.info;
+            log.consLog = console.log;
+            log.consDebug = console.debug;
+            console.info = function() {};
+            console.log = function() {};
+            console.debug = function() {};
+        } else {
+            log.level = 3;
+            log.enabled = true;
+            if (log.consInfo !== undefined) {
+                console.info = log.consInfo;
+                console.log = log.consLog;
+                console.debug = log.consDebug;
+            }
+        }
+    }
 };
 
-$(document).ready(function() {
-    if (!DEBUG) {
-        console.info("disabling all console log/info/debug messages");
-        console.info = function() {};
-        console.log = function() {};
-        console.debug = function() {};
-    }
-});
+log.resetDebug(DEBUG);
--- Pelilogiikka/src/test/javascript/common/Log_spec.js
+++ Pelilogiikka/src/test/javascript/common/Log_spec.js
@@ -1,21 +1,78 @@
-//describe('the Log object', function () {
-//
-//    //Create an easily-removed container for our tests to play in
-//    beforeEach(function () {
-//    });
-//
-//    //Clean it up after each spec
-//    afterEach(function () {
-//    });
-//
-//    //Specs
-//    describe('log tests', function () {
-//        it('logs error', function () {
-//            log.enabled = true;
-//            log.error("testing an error");
-//            expect(log.rows.pop()).toEqual("ERROR: testing an error");
-//        });
-//
-//    });
-//
-//});
+/*global log: true */
+/*global describe: true */
+/*global expect: true */
+/*global it: true */
+/*global spyOn: true */
+
+
+describe('log', function () {
+    "use strict";
+
+    describe('lever setter/getter', function () {
+        it('works', function () {
+            log.setLevel(1);
+            expect(log.getLevel()).toBe(1);
+            log.setLevel(3);
+            expect(log.getLevel()).toBe(3);
+        });
+    });
+
+    describe('log functions', function () {
+
+        it('error works without send', function () {
+            expect(function () {
+                log.error('error');
+            }).not.toThrow();
+        });
+
+        it('warn without send', function () {
+            expect(function() {
+                log.warn('warn');
+            }).not.toThrow();
+        });
+
+        it('info without send', function () {
+            expect(function() {
+                log.info('info');
+            }).not.toThrow();
+        });
+
+        it('debug without send', function () {
+            expect(function() {
+                log.debug('debug');
+            }).not.toThrow();
+        });
+
+        it('works with end', function () {
+            expect(function() {
+                log.error('error', true);
+                log.warn('warn', true);
+                log.info('info', true);
+                log.debug('debug', true);
+            }).not.toThrow();
+        });
+
+        it('thorwsToServer', function() {
+            expect(function () {
+                log.throwToServer(new Error("yay"));
+            }).not.toThrow();
+        });
+
+        it('resetDebug works', function () {
+            expect(function () {
+                log.resetDebug(true);
+                console.log("asdfsa");
+                console.info("asdfsa");
+                console.debug("asdfsa");
+                log.resetDebug(false);
+                console.log("asdfsa");
+                console.info("asdfsa");
+                console.debug("asdfsa");
+                log.resetDebug(true);
+                console.log("asdfsa");
+                console.info("asdfsa");
+                console.debug("asdfsa");
+            }).not.toThrow();
+        });
+    });
+});
