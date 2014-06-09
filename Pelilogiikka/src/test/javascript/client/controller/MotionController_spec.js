6a4044d353616a30d4fb3a102462d405969a6f81
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-09 16:14:47 +0300

    blaah

--- Pelilogiikka/src/test/javascript/client/controller/MotionController_spec.js
+++ Pelilogiikka/src/test/javascript/client/controller/MotionController_spec.js
@@ -5,9 +5,9 @@ describe('the MotionController (client) object', function () {
     var coms;
     var arr;
     var controllerDisabler;
-    
-    sendServerMessage = function(){
-        
+
+    sendServerMessage = function () {
+
     };
 
     //Create an easily-removed container for our tests to play in
@@ -20,38 +20,38 @@ describe('the MotionController (client) object', function () {
     });
 
     //Specs
-//    describe('MotionController tests', function () {
-//
-//        it('calls coms with the correct method and position parameters on device orientation', function () {
-//            canvas = document.createElement("canvas");
-//            canvas.width = 100;
-//            canvas.height = 100;
-//            coms = {
-//                call: function (method, params, object, callback) {
-//                }
-//            };
-//
-//            client.loadedTypes.motionController(null, canvas, null, coms, 20);
-//
-//            spyOn(coms, 'call');
-//            evt = document.createEvent("Events");
-//
-//            //Aim: initialize it to be the event we want     
-//            evt.initEvent('deviceorientation', true, true); //true for can bubble, true for cancelable
-//            data = {
-//                gamma: 10,
-//                beta: 20,
-//                alpha: 30
-//            };
-//            evt.eventData = data;
-//
-//            canvas.dispatchEvent(evt);
-//            arr = [10, 20, 30];
-//            expect(coms.call).toHaveBeenCalled();
-//            expect(coms.call).toHaveBeenCalledWith('orientation', arr, null, null);
-//
-//        });
-//    });
-    
-   
+    describe('MotionController tests', function () {
+
+        it('calls coms on device orientation', function () {
+            canvas = document.createElement("canvas");
+            canvas.width = 100;
+            canvas.height = 100;
+            coms = {
+                call: function (method, params, object, callback) {
+                }
+            };
+
+            client.loadedTypes.motionController(null, canvas, null, coms);
+
+            spyOn(coms, 'call');
+            spyOn(window, 'addEventListener');
+
+            evt = document.createEvent("Events");
+
+            evt.initEvent('deviceorientation', true, true, 10, 20, 30, 5); //true for can bubble, true for cancelable
+
+            var data = {
+                gamma : 10,
+                beta : 20,
+                alpha : 30
+            };
+            evt.eventData = data;
+            window.dispatchEvent(evt);
+
+            var arr = [10, 20, 30];
+            expect(coms.call).toHaveBeenCalled();
+
+        });
+    });
+
 });
