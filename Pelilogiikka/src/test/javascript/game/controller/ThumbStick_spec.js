d543e3045b8ad359d3ff71284387a429f0dbb53f
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-10 12:40:54 +0300

    test added for thumbstick game side

--- Pelilogiikka/src/test/javascript/game/controller/ThumbStick_spec.js
+++ Pelilogiikka/src/test/javascript/game/controller/ThumbStick_spec.js
@@ -1,15 +1,43 @@
-//describe('the ThumbStick object', function() {
-//
-//    //Create an easily-removed container for our tests to play in
-//    beforeEach(function() {
-//    });
-//
-//    //Clean it up after each spec
-//    afterEach(function() {
-//    });
-//
-//    //Specs
-////    describe('ThumbStick tests', function() {  
-////    });
-//
-//});
+describe('the ThumbStick (game) object', function () {
+    //Create an easily-removed container for our tests to play in
+
+    var mockPlayer = {
+        setPosition: function () {
+        },
+        shoot: function(){
+            
+        }
+    };
+    var mockRpc = {
+        exposeRpcMethod: function () {
+        }
+    };
+    var controllerObj;
+
+    beforeEach(function () {
+        controllerObj = controller.loadedTypes.ThumbStick.getController(mockPlayer, mockRpc);
+
+    });
+
+    //Clean it up after each spec
+    afterEach(function () {
+
+    });
+
+    //Specs
+    describe('ThumbStick (game) tests', function () {
+
+        it('resets player and calls exposeRpcMethod on reset', function () {
+
+            spyOn(mockRpc, 'exposeRpcMethod');
+
+            controllerObj.reset(mockPlayer, mockRpc);
+
+            expect(mockRpc.exposeRpcMethod).toHaveBeenCalled();
+            expect(mockRpc.exposeRpcMethod).toHaveBeenCalledWith('thumbStickPosition', controllerObj, controllerObj.thumbStickPosition);
+            
+
+        });
+
+    });
+});
