7e4e48cbd47880f84bddfc27031a261fdf4a4939
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-06-11 16:16:57 +0300

    tests

--- Pelilogiikka/src/test/javascript/rendering/math/Color_spec.js
+++ Pelilogiikka/src/test/javascript/rendering/math/Color_spec.js
@@ -1,15 +1,22 @@
-//describe('the Color object', function() {
-//
-//    //Create an easily-removed container for our tests to play in
-//    beforeEach(function() {
-//    });
-//
-//    //Clean it up after each spec
-//    afterEach(function() {
-//    });
-//
-//    //Specs
-////    describe('Color tests', function() {  
-////    });
-//
-//});
+describe('the Color object', function () {
+
+    //Create an easily-removed container for our tests to play in
+    beforeEach(function () {
+    });
+
+    //Clean it up after each spec
+    afterEach(function () {
+    });
+
+    //Specs
+    describe('Color tests', function () {
+        it('initializes with valid colors', function () {
+            var testColor = new Color(0.1, 0.2, 0.3, 0.4);
+            expect(testColor.red).toBe(0.1);
+            expect(testColor.green).toBe(0.2);
+            expect(testColor.blue).toBe(0.3);
+            expect(testColor.alpha).toBe(0.4);
+        });
+    });
+
+});
