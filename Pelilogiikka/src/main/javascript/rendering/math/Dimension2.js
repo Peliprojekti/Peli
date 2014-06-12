4925d3dac9f24391179940deb9a4f61ff0623ae6
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-06-12 15:24:50 +0300

    tests

--- Pelilogiikka/src/main/javascript/rendering/math/Dimension2.js
+++ Pelilogiikka/src/main/javascript/rendering/math/Dimension2.js
@@ -1,15 +1,11 @@
+function Dimension2 (width, height)
+{
+    ASSERT_POSITIVE(width, "Dimension must be positive: " + width);
+    ASSERT_POSITIVE(height, "Dimension must be positive: " + height);
 
-
-
-
-    function Dimension2( width, height )
-    {
-        ASSERT_POSITIVE( width  , "Dimension must be positive: " + width  );
-        ASSERT_POSITIVE( height , "Dimension must be positive: " + height );
-        
-        this.width  = width;
-        this.height = height;
-    }
+    this.width = width;
+    this.height = height;
+}
 
 
 
