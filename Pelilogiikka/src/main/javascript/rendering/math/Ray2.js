a9d139d9e4c216389bd17f18c6ad648b92de48eb
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-06-12 15:44:25 +0300

    tests

--- Pelilogiikka/src/main/javascript/rendering/math/Ray2.js
+++ Pelilogiikka/src/main/javascript/rendering/math/Ray2.js
@@ -1,10 +1,8 @@
-    
-
-    function Ray2( origin, direction )
-    {
-        this.origin    = origin;
-        this.direction = direction.normalized();
-    }
+function Ray2 (origin, direction)
+{
+    this.origin = origin;
+    this.direction = direction.normalized();
+}
 
 // A + tV        =    B + sW 
 //     tV - sW   =    B - A
@@ -12,35 +10,26 @@
 //   <t,s>M      =    B - A
 //   <t,s>       =   (B - A)M^-1
 
+Ray2.prototype.intersects = function (pointA, pointB)
+{
+    var A = this.origin;
+    var B = pointA;
+    var V = this.direction;
+    var W = pointB.subtract(pointA);
+    var matrix = new Matrix22([V.x, V.y,
+        -W.x, -W.y]);
+    var vec = pointA.subtract(this.origin);
+    if (!matrix.invertible())
+        return false;
+    var inverted = matrix.invert();
+    var params = inverted.transform(vec);
 
+    //  alert( params.x + " " + params.y );
 
-
-    Ray2.prototype.intersects = function( pointA, pointB )
-    {
-        var         A = this.origin;
-        var         B = pointA;
-        var         V = this.direction;
-        var         W = pointB.subtract( pointA );
-        
-        
-        var    matrix = new Matrix22( [  V.x , V.y,
-                                        -W.x , -W.y ] );
-        
-        var vec       = pointA.subtract( this.origin );
-       
-        if( !matrix.invertible() ) return false;
-        
-        var inverted = matrix.invert();
-        var params   = inverted.transform( vec );
-        
-      //  alert( params.x + " " + params.y );
-        
-        if( params.x >= 0.0 )   // Intersection is on Ray 
-            if( params.y >= 0.0 && params.y <= 1.0 )    // Intersection is on the target segment
-            {
-                return true;
-            }
-
-        
+    if (params.x >= 0.0)   // Intersection is on Ray 
+        if (params.y >= 0.0 && params.y <= 1.0)    // Intersection is on the target segment
+        {
+            return true;
+        }
     return false;
-    }
+}
--- Pelilogiikka/src/main/javascript/rendering/math/Ray3.js
+++ Pelilogiikka/src/main/javascript/rendering/math/Ray3.js
@@ -1,32 +1,25 @@
+function Plane3 (point, normal)
+{
+    this.point = point;
+    this.normal = normal;
+}
 
+Plane3.prototype.rotate = function (mat33)
+{
+    ASSERT_TYPE(Matrix33, mat33);
+    this.normal = mat33.transform(this.normal);
+}
 
-    function Plane3( point, normal )
+function Ray3 (origin, direction)
+{
+    this.origin = origin;
+    this.direction = direction.normalized();
+}
+
+Ray3.prototype.intersects = function (thing)
+{
+    if (TYPE(Sphere3, thing))
     {
-        this.point  = point;
-        this.normal = normal;
-    }
-
-    Plane3.prototype.rotate = function( mat33 )
-    {
-        ASSERT_TYPE( Matrix33, mat33 );
-        this.normal = mat33.transform( this.normal );
-    }
-
-
-
-    function Ray3( origin, direction )
-    {
-        this.origin    = origin;
-        this.direction = direction.normalized();
-    }
-    
-    
-    Ray3.prototype.intersects = function( thing )
-    {
-        
-        if( TYPE( Sphere3 , thing ) )
-        {
-            
         // ( A + tV - O )^2  = r^2
         // ( tV + (A-O) )^2  = r^2
         // tV*tV   + 2*tV(A-O)   + (A-O)*(A-O) = r^2
@@ -34,47 +27,36 @@
         // t^2(V*V) + 2t(V*(A-O)) + (A-O)*(A-O) - r^2 = 0
         // (t^2)X + (t)2Y + Z = 0
         //  t = -Y +- sqrt( Y^2-4XZ)/2X     
-       
-            var A  = this.origin;
-            var V  = this.direction;
-            var O  = thing.origin;
-            var r  = thing.radius;
-           
-            var X  = V.dot( V );
-            var Y  = 2.0 * V.dot( A.subtract( O ) );
-            var Z  = A.subtract( O ).dot( A.subtract( O ) ) - r*r;
-            
-            
-            var discriminant = Y*Y - 4*X*Z;
-            
-            if( discriminant < 0 ) 
-            {
-           //     alert("Disciminant: " + discriminant);
-                return false;
-            }
-            
-            
-            var t1 = ( -Y + Math.sqrt( discriminant  ) ) / 2*X;
-            var t2 = ( -Y - Math.sqrt( discriminant )  ) / 2*X;
-             
-        
-            if( t1 > 0 || t2 > 0 )
-            {
-                return true;
-            }
-            
-        
-        return false;
-        }
-        
-        
-        if( TYPE( Plane3 , thing ) )
+
+        var A = this.origin;
+        var V = this.direction;
+        var O = thing.origin;
+        var r = thing.radius;
+
+        var X = V.dot(V);
+        var Y = 2.0 * V.dot(A.subtract(O));
+        var Z = A.subtract(O).dot(A.subtract(O)) - r * r;
+
+        var discriminant = Y * Y - 4 * X * Z;
+
+        if (discriminant < 0)
         {
-            
-            
-        return;
+            //     alert("Disciminant: " + discriminant);
+            return false;
         }
         
+        var t1 = (-Y + Math.sqrt(discriminant)) / 2 * X;
+        var t2 = (-Y - Math.sqrt(discriminant)) / 2 * X;
         
-        
+        if (t1 > 0 || t2 > 0)
+        {
+            return true;
+        }
+        return false;
     }
+
+    if (TYPE(Plane3, thing))
+    {
+        return;
+    }
+}
