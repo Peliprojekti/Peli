30da4d0f12bf6652216819db6b86f6b0430e487f
ishkamiel <ishkamiel@gmail.com>
2014-06-12 14:44:13 +0300

    hopefully fixed error

--- Pelilogiikka/src/main/data/Fairground/convert.sh
+++ Pelilogiikka/src/main/data/Fairground/convert.sh
@@ -7,6 +7,7 @@ for i in $( find -iname '*.irrmesh' -or -iname '*.irr'); do
     else 
         #iconv -f UTF-16 -t UTF-8 3.irrmesh -o 3.irrmesh
         iconv -f UTF-16 -t UTF-8 $i -o $i
-        echo iconv -f UTF-16 -t UTF-8 $i -o $i
+        echo iconv -f UTF-16 -t UTF-8 $i -o $i.tmp
+        mv $i.tmp $i
     fi
 done
