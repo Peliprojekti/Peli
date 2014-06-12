0ae2c6d3f4d677afa1cf6bebe2945ec0ae02e649
ishkamiel <ishkamiel@gmail.com>
2014-06-12 15:06:29 +0300

    convertteri säätöä, ankat takas

--- Pelilogiikka/src/main/data/Fairground/convert.sh
+++ Pelilogiikka/src/main/data/Fairground/convert.sh
@@ -6,8 +6,8 @@ for i in $( find -iname '*.irrmesh' -or -iname '*.irr'); do
         echo skipping: $i
     else 
         #iconv -f UTF-16 -t UTF-8 3.irrmesh -o 3.irrmesh
-        iconv -f UTF-16 -t UTF-8 $i -o $i
-        echo iconv -f UTF-16 -t UTF-8 $i -o $i.tmp
+        echo iconv -f UTF-16 -t UTF-8 $i -o $i
+        iconv -f UTF-16 -t UTF-8 $i -o $i.tmp
         mv $i.tmp $i
     fi
 done
