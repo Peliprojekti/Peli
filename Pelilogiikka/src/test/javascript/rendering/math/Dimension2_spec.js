e37051e9afa0beb75e707fb963a0d60cc11c1b8f
lairala <lairala@da1-cs-b221-23.helsinki.fi>
2014-06-12 15:28:10 +0300

    tests

--- Pelilogiikka/src/test/javascript/rendering/math/Dimension2_spec.js
+++ Pelilogiikka/src/test/javascript/rendering/math/Dimension2_spec.js
@@ -1,15 +1,19 @@
-//describe('the Dimension2 object', function() {
-//
-//    //Create an easily-removed container for our tests to play in
-//    beforeEach(function() {
-//    });
-//
-//    //Clean it up after each spec
-//    afterEach(function() {
-//    });
-//
-//    //Specs
-////    describe('Dimension2 tests', function() {  
-////    });
-//
-//});
+describe('the Dimension2 object', function() {
+
+    //Create an easily-removed container for our tests to play in
+    beforeEach(function() {
+    });
+
+    //Clean it up after each spec
+    afterEach(function() {
+    });
+
+    //Specs
+    describe('Dimension2 tests', function() {
+        it('it initializes the dimensions correctly', function () {
+            var dimension = new Dimension2(0.5,1.5);
+            expect(dimension.width).toBe(0.5);
+            expect(dimension.height).toBe(1.5);
+        });
+    });
+});
