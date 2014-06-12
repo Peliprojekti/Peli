84ec968de262bc05b9d65750f250bb505b2f5d53
pete <pcmakine@da1-cs-b221-18.helsinki.fi>
2014-06-12 12:38:08 +0300

    targetmanager and playermanager done

--- Pelilogiikka/src/main/javascript/game/renderingPeli/scene.js
+++ Pelilogiikka/src/main/javascript/game/renderingPeli/scene.js
@@ -26,6 +26,7 @@ renderingPeli.scene = {
         this.camera = new Camera(new Vector3(95.65460642017462, 10, -34.54491692528497));
         this.camera.yaw(180);
         this.world = new World('Fairground');
+        renderingPeli.targetManager.initialize(this.world);
 
         this.registerKeyboardListeners();
 
@@ -34,9 +35,7 @@ renderingPeli.scene = {
     animate: function (time) {
         "use strict";
 
-        this.players.forEach(function (p) {
-            p.update(time);
-        });
+        renderingPeli.playerManager.updatePlayers(time);
 
         this.rendererDraw();
         requestAnimationFrame(this.animate.bind(this));
@@ -44,75 +43,72 @@ renderingPeli.scene = {
     rendererDraw: function () {
         "use strict";
 
-        var duck = this.world.get_Targets()[0],
-            player, 
+        var player,
             i;
 
         this.renderer.set_Camera(this.camera);
         this.renderer.begin();
 
-        this.renderer.set_Matrices( null, this.camera.get_ViewMatrix(), this.camera.get_ProjectionMatrix() );
+        this.renderer.set_Matrices(null, this.camera.get_ViewMatrix(), this.camera.get_ProjectionMatrix());
 
         this.world.render(this.camera);
 
-        duck.set_Stage((this.duckPos++ % 150) / 150);
-        
+        renderingPeli.targetManager.updateTargetPositions(time);
+
         for (i = 0; i < this.players.length; i++) {
-            player = this.players[i];
+            player = renderingPeli.playerManager.players[i];
 
             this.renderer.set_Shader(player.shader);
             var trans = player.guiItem.get_Transformation();
             this.renderer.set_Matrices(trans, null, null);
             this.renderer.draw_Batch(player.guiItem.batch);
-        };
+        }
+        ;
     },
     addPlayer: function (player) {
-        "use strict";
-        console.info("scene::addPlayer - attaching player to scene");
-
-        this.crosshair_id = (this.crosshair_id + 1) % 8;
-        var texture = new Texture('/data/crosshairs/crosshair' + this.crosshair_id + '.png');
-
-        player.shader = new GuiShader();
-        player.guiItem = new GuiItem(new Vector2(0, 0), new Dimension2(0.07, 0.07), texture);
-        player.setCrosshairID(this.crosshair_id);
-        this.players.push(player);
+        renderingPeli.playerManager.addPlayer(player);
     },
     removePlayer: function (player) {
-        "use strict";
-
-        this.players = this.players.filter(function (p) {
-            return (p === player ? false : true);
-        });
-        
-        delete player.guiItem;
-        delete player.shader;
+        renderingPeli.playerManager.removePlayer(player);
     },
     registerKeyboardListeners: function () {
         "use strict";
         var self = this;
-        
+
         document.onkeydown = function (e) {
             switch (e.keyCode) {
-                case 38: 
+                case 38:
                     self.camera.forward(2.0);
                     break;
                 case 40:
                     self.camera.backwards(2.0);
                     break;
-                case 37: 
+                case 37:
                     self.camera.yaw(-2.0);
                     break;
-                case 39: 
+                case 39:
                     self.camera.yaw(2.0);
                     break;
-                case 39: 
+                case 39:
                     self.camera.yaw(2.0);
                     break;
-                case 32: 
+                case 32:
                     console.debug("camera position", self.camera.position.x, self.camera.position.y, self.camera.position.z)
                     break;
             }
         }
+    },
+    /**
+     * 
+     * @param {Vector2} position vector of the shot position
+     * @param {Player} the player who shot
+     * @returns {boolean} was it a hit or not
+     */
+    shoot: function (position, player) {
+        if (renderingPeli.targetManager.shoot(position)) {
+            renderingPeli.playerManager.addPlayerScore(player);
+            return true;
+        }
+        return false;
     }
 };
